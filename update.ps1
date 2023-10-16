c61d999d5d9468b4f79b20435e271d96af2de663
Bill Avery <wravery@gmail.com>
2023-10-15 18:40:26 -0700

    Fix path to match webview2-rs project layout

--- update.ps1
+++ update.ps1
@@ -2,4 +2,4 @@ param($webview2_rs_path)
 
 & dotnet build
 
-Copy-Item .\bin\Microsoft.Web.WebView2.Win32.winmd "$webview2_rs_path\crates\bindings\winmd\"
+Copy-Item .\bin\Microsoft.Web.WebView2.Win32.winmd "$webview2_rs_path\crates\update-bindings\winmd\"
